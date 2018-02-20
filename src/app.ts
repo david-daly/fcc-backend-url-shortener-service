0081210e25c930aa4bc2449e9028aed73ce1e060
david-daly <david-daly@live.ie>
2018-02-20 22:29:58 +0000

    initial code to save url that is sent via the body

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
+import { Connection, createConnection, Mongoose } from 'mongoose';
+import { UrlModel } from './models/UrlModel';
 import { Response } from './utils/Response';
 
-export const handler = (event, context, callback) => {
+const mongoConnection = createConnection('mongodb://localhost:27017/url');
 
-    console.log(JSON.parse(event.body));
-    context.succeed(new Response(200, 'success'));
+export const handler = (event, context, callback) => {
+    const url = mongoConnection.model(UrlModel.name, UrlModel.schema);
+    url.create(JSON.parse(event.body), (err, shortUrl) => {
+        if (err) {
+            context.fail(500, JSON.stringify(err));
+        } else {
+            context.succeed(new Response(200, JSON.stringify(shortUrl)));
+        }
+    });
 };
